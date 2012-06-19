9e61e7c8fb0c5709b00d95c31b8bffa7c938a955
Fran <avernumcito@hotmail.com>
2012-06-19 15:25:18 +0200

    Commit test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assoftnetweb::Application.config.secret_token = 'bffc7f5085868aa7eb94f5a3de78a89fe2e98e7d042122d8b0c56b96d4a306e783fcca6ba21e5ad1e2a620ff51c0e693358b0ca4f857e93a8c42d1c12da97602'
